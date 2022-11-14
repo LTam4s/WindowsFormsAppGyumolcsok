099419c60a11b02de8307f21c58d4da79851ccf1
LTam4s <114070808+LTam4s@users.noreply.github.com>
2022-11-14 11:10:14 +0100

    Add project files.

---
+++ WindowsFormsAppGyumolcsok/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace WindowsFormsAppGyumolcsok
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxId = new System.Windows.Forms.TextBox();
+            this.textBoxNev = new System.Windows.Forms.TextBox();
+            this.numericUpDownEgysegar = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDownMennyiseg = new System.Windows.Forms.NumericUpDown();
+            this.insert = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownEgysegar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownMennyiseg)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(0, 0);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(250, 373);
+            this.listBox1.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.insert);
+            this.groupBox1.Controls.Add(this.numericUpDownMennyiseg);
+            this.groupBox1.Controls.Add(this.numericUpDownEgysegar);
+            this.groupBox1.Controls.Add(this.textBoxNev);
+            this.groupBox1.Controls.Add(this.textBoxId);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox1.Location = new System.Drawing.Point(250, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(276, 373);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Gyümölcsök";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(65, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(15, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "id";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(55, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(25, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "név";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(30, 118);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "egységár";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(23, 156);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(57, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "mennyiség";
+            // 
+            // textBoxId
+            // 
+            this.textBoxId.Location = new System.Drawing.Point(93, 38);
+            this.textBoxId.Name = "textBoxId";
+            this.textBoxId.ReadOnly = true;
+            this.textBoxId.Size = new System.Drawing.Size(100, 20);
+            this.textBoxId.TabIndex = 4;
+            // 
+            // textBoxNev
+            // 
+            this.textBoxNev.Location = new System.Drawing.Point(93, 76);
+            this.textBoxNev.Name = "textBoxNev";
+            this.textBoxNev.Size = new System.Drawing.Size(100, 20);
+            this.textBoxNev.TabIndex = 5;
+            // 
+            // numericUpDownEgysegar
+            // 
+            this.numericUpDownEgysegar.Location = new System.Drawing.Point(93, 116);
+            this.numericUpDownEgysegar.Name = "numericUpDownEgysegar";
+            this.numericUpDownEgysegar.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDownEgysegar.TabIndex = 6;
+            // 
+            // numericUpDownMennyiseg
+            // 
+            this.numericUpDownMennyiseg.Location = new System.Drawing.Point(93, 154);
+            this.numericUpDownMennyiseg.Name = "numericUpDownMennyiseg";
+            this.numericUpDownMennyiseg.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDownMennyiseg.TabIndex = 7;
+            // 
+            // insert
+            // 
+            this.insert.Location = new System.Drawing.Point(26, 196);
+            this.insert.Name = "insert";
+            this.insert.Size = new System.Drawing.Size(187, 36);
+            this.insert.TabIndex = 8;
+            this.insert.Text = "Új gyümölcs hozzáadása";
+            this.insert.UseVisualStyleBackColor = true;
+            this.insert.Click += new System.EventHandler(this.insert_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(526, 373);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Gyümölcsök";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownEgysegar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownMennyiseg)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button insert;
+        private System.Windows.Forms.NumericUpDown numericUpDownMennyiseg;
+        private System.Windows.Forms.NumericUpDown numericUpDownEgysegar;
+        private System.Windows.Forms.TextBox textBoxNev;
+        private System.Windows.Forms.TextBox textBoxId;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
